00ae90ccbe61a12ae80c75a205e3276d23186c91
Alinka.Sergei <alinka.sergei@gmail.com>
2022-06-30 21:06:38 +0300

    done

---
+++ Desktop/domashka2/Program.cs
@@ -0,0 +1,53 @@
+﻿/*int n;
+void SecondDigit()
+{
+    Console.WriteLine("input a number ");
+    n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Current number is " + n);
+
+    int ed = n / 10 % 10;
+  
+    System.Console.WriteLine("SecondDigit is " + ed);
+}
+SecondDigit();
+
+
+int n;
+void ShowThreeDigit()
+{
+    Console.WriteLine("input a number ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string nText = Convert.ToString(n);
+if (nText.Length > 2)
+    Console.WriteLine("Three digit is " + nText[2]);
+else
+    Console.WriteLine("Three digit is absent");
+  
+}
+ShowThreeDigit(); 
+*/
+
+
+/*int dayNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a dayNum: ");
+void DayOfTheWeek(int dayNum)
+{
+    Console.WriteLine (dayNum);
+
+  if (dayNum == 6) 
+  {
+    Console.WriteLine("This day is off");
+  }
+  else if (dayNum == 7)
+  {
+    Console.WriteLine("This day is off");
+  }
+  else
+    {
+    Console.WriteLine("This day isn't off");
+    }
+}
+DayOfTheWeek(dayNum);
+*/
+
+
